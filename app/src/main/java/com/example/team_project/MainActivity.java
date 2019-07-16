d9885c3129833c9ec80c07e1c35e2c4e9b32f89b
Cecilia Baek <cbaek@fb.com>
2019-07-16 14:25:22 -0700

    "set up firebase - CB"

--- app/src/main/java/com/example/team_project/MainActivity.java
+++ app/src/main/java/com/example/team_project/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,11 +15,18 @@ import com.example.team_project.fragments.ComposeFragment;
 import com.example.team_project.fragments.NotificationFragment;
 import com.example.team_project.fragments.PostsFragment;
 import com.example.team_project.fragments.ProfileFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MESSENGER_REQUEST_CODE = 100;
     private BottomNavigationView mBottomNavigationView;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     Fragment fragment;
 
@@ -26,6 +34,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("MainActivity", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("MainActivity", "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+
         final FragmentManager fragmentManager = getSupportFragmentManager();
         // handle bottom navigation selection
         mBottomNavigationView = findViewById(R.id.bottom_navigation);
