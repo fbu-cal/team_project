71619e1e141b6aca9596d2565463124dd71adfb5
Cecilia Baek <cbaek@fb.com>
2019-07-17 15:33:59 -0700

    "updated models and progress on camera"

--- app/src/main/java/com/example/team_project/models/User.java
+++ app/src/main/java/com/example/team_project/models/User.java
@@ -1,19 +1,28 @@
 package com.example.team_project.models;
 
-public class User {
-    String username;
-    String email;
-    String password;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String username;
+    private String email;
+    private String password;
+    private String photoUrl;
+    private String uid;
+    private Calendar calendar;
+
 
     public User() {
         //Empty Constructor For Firebase
     }
 
-    public User(String username, String email, String password)
+    public User(String username, String email, String password, String photoUrl, String uid, Calendar calendar)
     {
         this.username = username; //Parameterized for Program-Inhouse objects.
         this.email = email;
         this.password = password;
+        this.photoUrl = photoUrl;
+        this.uid = uid;
+        this.calendar = calendar;
     }
 
     //Getters and Setters
@@ -40,5 +49,29 @@ public class User {
     public void setPassword(String password)
     {
         this.password = password;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
     }
 }
