9ebe2430e50ee243cccfcbffb73fa723679cd0b4
Alexei Tulloch <a0tulloch1@gmail.com>
2019-07-22 14:53:00 -0700

    Update FirstActivity.java
    removed androidx

--- app/src/main/java/com/example/team_project/FirstActivity.java
+++ app/src/main/java/com/example/team_project/FirstActivity.java
@@ -1,7 +1,7 @@
 package com.example.team_project;
 
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
